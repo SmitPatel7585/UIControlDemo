e801dd548c576c274b671abc5710b92dbb3133e8
Smit Patel <smit.patel7585@gmail.com>
2024-01-22 12:36:34 -0500

    Initial Commit

---
+++ UIControlDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIControlDemo
+//
+//  Created by user235217 on 1/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
