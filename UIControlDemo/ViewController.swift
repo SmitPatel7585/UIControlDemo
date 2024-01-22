ef1588af63a91219d5b1562b29f2118f4a7df8b3
Smit Patel <smit.patel7585@gmail.com>
2024-01-22 14:44:20 -0500

    first commit

--- UIControlDemo/ViewController.swift
+++ UIControlDemo/ViewController.swift
@@ -9,6 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter = 0
+    var flag = 0
+    
+    @IBOutlet weak var label_counter: UILabel!
+    
+    @IBAction func btnDecrease(_ sender: Any){
+        if(flag == 0){
+            counter -= 1
+            label_counter.text = String(counter)
+        }else if(flag == 1){
+            counter -= 2
+            label_counter.text = String(counter)
+        }
+    }
+    
+    @IBAction func btnIncrease(_ sender: Any){
+        
+        if(flag == 0){
+            counter += 1
+            label_counter.text = String(counter)
+        }else if(flag == 1){
+            counter += 2
+            label_counter.text = String(counter)
+        }
+    }
+    
+    
+    @IBAction func btnReset(_ sender: Any) {
+        
+        label_counter.text = String(0)
+        flag = 0;
+        counter = 0;
+    }
+    
+    @IBAction func btnDouble(_ sender: Any) {
+        
+        flag = 1;
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,4 +56,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
